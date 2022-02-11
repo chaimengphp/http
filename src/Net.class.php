9d79811552f4a85a27a7756f7c1a289be3b24638
柴萌 <chaimengmeng@jiemian.com>
2022-02-11 15:35:03 +0800

    第一次提交代码

---
+++ src/Net.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chaimeng
+ * Date: 2022/2/11
+ * Time: 15:16
+ */
+
+class Net {
+    public function curl($url)
+    {
+        $content = file_get_contents($url);
+        return $content;
+    }
+}
